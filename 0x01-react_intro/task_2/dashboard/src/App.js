76f974b657dc62b9c77ab2e7a1144bab782a5082
mystry22 <alanemehenry6@gmail.com>
2024-05-17 04:01:22 +0100

    starting test for getFooterCopy on utils.js

---
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,39 @@
+
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+// import fav_icon from './fav_icon.ico';
+import holbert_logo from './holbert_logo.jpg';
+import Notification from './Notifications';
+
+function App() {
+  return (
+    <div className="App">
+      <Notification />
+      <header className="App-header">
+      <img src={holbert_logo} alt="Holberton Logo" />
+        <h1> School dashboard</h1>
+      </header>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+          {/* Email Input */}
+          <div>
+            <label htmlFor="email">Email: </label>
+              <input type="email" id="email" />            
+          </div>
+          <div>
+            <label htmlFor="password">Password: </label>
+            <input type="password" id="password" />
+          </div>
+
+          {/* OK Button */}
+          <button>OK</button>
+      </div>
+      <footer className="App-footer">
+        {getFooterCopy(true)}
+        <p>{getFullYear()}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
